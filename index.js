668d8d664feca745ca9340f8c604d684d6b2cf15
Justin Navarro <info@justinnavarro.net>
2019-07-03 17:33:15 -0400

    created logger middleware

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const logger = (req, res, next) => {
+    console.log('hello');
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+};
+
+app.use(logger);
+
 app.get('/api/members', (req, res) => {
     const members = [
         {
