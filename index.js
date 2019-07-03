dd90b4639a877081db5a4bc51b633d9ebc1fe9c5
Justin Navarro <info@justinnavarro.net>
2019-07-03 17:12:44 -0400

    installed nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('hello motto');
+    res.send('hello motto!!!!');
 });
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
