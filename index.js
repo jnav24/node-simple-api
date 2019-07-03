a85f177f8183854f29a6d2e29b3b01ae7a41cd1e
Justin Navarro <info@justinnavarro.net>
2019-07-03 17:19:37 -0400

    updated get endpoint to express use

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(path.join(__dirname, 'public', 'index.html')));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
