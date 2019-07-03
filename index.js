df3aa06b03cbfa70dd66dca5ecdd415f6828ab68
Justin Navarro <info@justinnavarro.net>
2019-07-03 17:38:44 -0400

    created models and middlewares directories and export some functionality

--- index.js
+++ index.js
@@ -1,39 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const moment = require('moment');
+const logger = require('./middleware/logger');
+const members = require('./models/members');
 const PORT = process.env.PORT || 5000;
-
-const logger = (req, res, next) => {
-    console.log('hello');
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`);
-    next();
-};
 
 app.use(logger);
 
 app.get('/api/members', (req, res) => {
-    const members = [
-        {
-            id: 1,
-            name: 'Steve Rogers',
-            email: 'srogers@avengers.com',
-            status: 'active',
-        },
-        {
-            id: 2,
-            name: 'Peter Parker',
-            email: 'pparker@dailybugle.com',
-            status: 'active',
-        },
-        {
-            id: 3,
-            name: 'Bruce Banner',
-            email: 'bbanner@avengers.com',
-            status: 'active',
-        },
-    ];
-
     res.json(members);
 });
 
---
+++ models/members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Steve Rogers',
+        email: 'srogers@avengers.com',
+        status: 'active',
+    },
+    {
+        id: 2,
+        name: 'Peter Parker',
+        email: 'pparker@dailybugle.com',
+        status: 'active',
+    },
+    {
+        id: 3,
+        name: 'Bruce Banner',
+        email: 'bbanner@avengers.com',
+        status: 'active',
+    },
+];
+
+module.exports = members;
