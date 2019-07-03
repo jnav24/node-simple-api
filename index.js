0515722aedd11a2f70e8829a2d03854abccf2ea1
Justin Navarro <info@justinnavarro.net>
2019-07-03 17:16:48 -0400

    get a file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('hello motto!!!!');
+    res.sendFile(path.join(path.join(__dirname, 'public', 'index.html')));
 });
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
