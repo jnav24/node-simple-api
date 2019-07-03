5e1c52400128af8c846bab953f1a93d8a2eaf7f0
Justin Navarro <info@justinnavarro.net>
2019-07-03 17:51:39 -0400

    create more sample routing

--- index.js
+++ index.js
@@ -5,10 +5,26 @@ const logger = require('./middleware/logger');
 const members = require('./models/members');
 const PORT = process.env.PORT || 5000;
 
-app.use(logger);
+// app.use(logger);
 
 app.get('/api/members', (req, res) => {
     res.json(members);
+});
+
+app.get('/api/members/:id', (req, res) => {
+    const index = members.findIndex((member) => Number(req.params.id) === Number(member.id));
+
+    if (index > -1) {
+        return res.status(200).json({
+            data: members[index],
+            msg: '',
+        });
+    }
+
+    res.status(400).json({
+        data: {},
+        msg: `Member is not found`,
+    });
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
