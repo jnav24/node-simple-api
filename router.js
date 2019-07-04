8861281dcc54f83eb42af5090c581036411ea874
Justin Navarro <info@justinnavarro.net>
2019-07-03 21:46:15 -0400

    create put endpoint to update members

--- router.js
+++ router.js
@@ -36,4 +36,30 @@ router.post('/', (req, res) => {
     res.json(members);
 });
 
+router.put('/:id', (req, res) => {
+    const index = members.findIndex((member) => Number(req.params.id) === Number(member.id));
+
+    if (index > -1) {
+        const acceptedParams = ['name', 'email'];
+        const updatedMember = {};
+
+        for (const param in acceptedParams) {
+            if (typeof req.body[acceptedParams[param]] !== 'undefined') {
+                updatedMember[acceptedParams[param]] = req.body[acceptedParams[param]];
+            }
+        }
+
+        members[index] = Object.assign({}, members[index], updatedMember);
+        return res.status(200).json({
+            data: members[index],
+            msg: 'Member has been updated successfully',
+        });
+    }
+
+    res.status(400).json({
+        data: {},
+        msg: `Member is not found`,
+    });
+});
+
 module.exports = router;
